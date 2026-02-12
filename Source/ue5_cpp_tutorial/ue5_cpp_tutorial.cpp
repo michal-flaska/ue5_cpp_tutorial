ece871bbe88c8b3ff5796077f7bfef35c2ee4914
Michal Flaška <109202734+pilot2254@users.noreply.github.com>
2026-02-12 11:53:43 +0100

    create new BLANK cpp project in ue5.6

---
+++ Source/ue5_cpp_tutorial/ue5_cpp_tutorial.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ue5_cpp_tutorial.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ue5_cpp_tutorial, "ue5_cpp_tutorial" );
