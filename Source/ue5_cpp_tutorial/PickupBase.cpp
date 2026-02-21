5a3c21dd2fbfaf94c9248a737e800269814f9b96
Michal Flaška <109202734+pilot2254@users.noreply.github.com>
2026-02-21 17:36:06 +0100

    create pickup item

---
+++ Source/ue5_cpp_tutorial/PickupBase.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PickupBase.h"
+
+// Sets default values
+APickupBase::APickupBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	RootComponent = Mesh;
+
+	InteractionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("InteractionSphere"));
+	InteractionSphere->SetupAttachment(RootComponent);
+	// InteractionSphere->SetSphereRadius(InteractionRadius); //this was moved to onconstruction func
+
+}
+
+void APickupBase::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	InteractionSphere->SetSphereRadius(InteractionRadius);
+}
+
+// Called when the game starts or when spawned
+void APickupBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APickupBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/ue5_cpp_tutorial/PickupBase.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "Components/SphereComponent.h"
+
+#include "PickupBase.generated.h"
+
+UCLASS()
+class UE5_CPP_TUTORIAL_API APickupBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APickupBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, Category = "! Mike's PickupBase|Pickup")
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "! Mike's PickupBase|Pickup")
+	USphereComponent* InteractionSphere;
+
+	UPROPERTY(EditDefaultsOnly, Category = "! Mike's PickupBase|Pickup")
+	float InteractionRadius = 200.f;
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+};
